1720af6b9edae942e85ded828fef4ba149f10cf6
Jantalus <mati.giampaolo@gmail.com>
2020-04-19 16:34:07 -0300

    repo inicial vacio

---
+++ cmake-build-debug/CMakeFiles/Algo3TP1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Algo3TP1.dir/main.cpp.obj"
+  "Algo3TP1.pdb"
+  "Algo3TP1.exe"
+  "Algo3TP1.exe.manifest"
+  "libAlgo3TP1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Algo3TP1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
