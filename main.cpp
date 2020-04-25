e9080f079fc9d8640903c02040bd9741743f3b42
crinigi <nachocrinigan@gmail.com>
2020-04-25 11:46:29 -0300

    BT parcial

--- main.cpp
+++ main.cpp
@@ -26,10 +26,29 @@ int FB(int i, int r, int k){
     }
     return(max(FB(i+1, r, k), FB(i+1, min(r - S[i], RS[i]), k+1)));
 }
+int K = INFINITO;
+int BT(int i, int r, int k){
+    if(i == n){
+        if(k < K){K = k;}
+        return (r < 0)? INFINITO:K;//devuelvo el K grande
+    }
+    if(poda_factibilidad && r < 0){
+        return INFINITO;
+    }
 
+    if(poda_optimalidad){
+
+        if(k+(S.size() -1 -i) <= K){
+            return INFINITO;
+        }
+    }
+
+    return(max(BT(i+1, r, k), BT(i+1, min(r - S[i], RS[i]), k+1)));
+}
 
 int main(int argc, char** argv){
-    map<string, string> algoritmosImplementados = {{"FB", "Fuerza Bruta"}};
+    map<string, string> algoritmosImplementados = {{"FB", "Fuerza Bruta"},{"BT", "Backtracking"},{"BT-F", "Backtracking con poda por factibilidad"}
+    , {"BT-O", "Backtracking con poda por optimilidad"}};
 
     if(argc < 2 || algoritmosImplementados.find(argv[1]) == algoritmosImplementados.end()){
         cerr << "Algoritmo no encontrado: " << argv[1] << endl;
@@ -48,6 +67,20 @@ int main(int argc, char** argv){
     int res = INFINITO;
     if(algoritmo == "FB"){
         res = FB(0,R,0);
+    }
+    if(algoritmo == "BT"){
+        poda_factibilidad = true;
+        res = BT(0,R,0);
+    }
+    if(algoritmo == "BT-F"){
+        poda_factibilidad = true;
+        poda_optimalidad = false;
+        res = BT(0,R,0);
+    }
+    if(algoritmo == "BT-O"){
+        poda_factibilidad = false;
+        poda_optimalidad = true;
+        res = BT(0,R,0);
     }
     cout << (res == INFINITO? -1 : res) << endl;
 
